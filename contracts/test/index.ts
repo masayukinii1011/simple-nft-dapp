7901e0bef988688d64b55583dad084f66ccd4fd9
masayukinii1011 <masayukinii1011@gmail.com>
2022-04-24 14:43:03 +0900

    fix test

--- contracts/test/index.ts
+++ contracts/test/index.ts
@@ -3,14 +3,12 @@ import { ethers } from "hardhat";
 
 describe("NFT", function () {
   it("NFT basic test", async function () {
-    const [signer, badSigner] = await ethers.getSigners();
+    this.timeout(60 * 1000);
+    const [signer] = await ethers.getSigners();
     const NFT = await ethers.getContractFactory("NFT");
     const nft = await NFT.deploy();
-    expect(await nft.name()).to.equal("name");
-    await nft.mint(signer.address);
+    expect(await nft.name()).to.equal("MyNFT");
+    await nft.mintNFT(signer.address, "tokenURI");
     expect(await nft.balanceOf(signer.address)).to.equal(1);
-    await expect(nft.connect(badSigner).mint(signer.address)).to.revertedWith(
-      "ERC721PresetMinterPauserAutoId: must have minter role to mint"
-    );
   });
 });
